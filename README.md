7cc24f506f85fe2165e2fb539e1e608fc8b3ce97
Phil Winder <phil@winderresearch.com>
2016-08-10 14:56:06 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cart
-Carts service for microservices-demo application
+A microservices-demo service that provides user account information.
+
+This build is built, tested and released by travis.
+
+# Test
+`./test/test.sh < python testing file >`. For example: `./test/test.sh unit.py`
+
+# Build
+`GROUP=weaveworksdemos COMMIT=test ./scripts/build.sh`
+
+# Push
+`GROUP=weaveworksdemos COMMIT=test ./scripts/push.sh`
---
+++ src/main/java/works/weave/socks/cart/entities/Cart.java
@@ -0,0 +1,70 @@
+package works.weave.socks.cart.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Document
+public class Cart {
+    @NotNull
+    public String customerId; // Public instead of getters/setters.
+    @Id
+    private String id;
+    @DBRef
+    private List<Item> items = new ArrayList<>();
+
+    public Cart(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public Cart() {
+        this(null);
+    }
+
+    public List<Item> contents() {
+        return items;
+    }
+
+    public Cart add(Item item) {
+        items.add(item);
+        return this;
+    }
+
+    public Cart remove(Item item) {
+        items.remove(item);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "id='" + id + '\'' +
+                ", customerId='" + customerId + '\'' +
+                ", items=" + items +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cart cart = (Cart) o;
+
+        if (customerId != null ? !customerId.equals(cart.customerId) : cart.customerId != null) return false;
+        if (id != null ? !id.equals(cart.id) : cart.id != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = customerId != null ? customerId.hashCode() : 0;
+        result = 31 * result + (id != null ? id.hashCode() : 0);
+        return result;
+    }
+}
